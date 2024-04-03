ae760e011e0309fd012e6b8b5066c7915546cbd1
Jonathan Frelispradana Musila <55726862+jfrelis@users.noreply.github.com>
2024-04-03 20:43:37 +0700

    Update home-page-locators.js

--- cypress/support/locators/home-page-locators.js
+++ cypress/support/locators/home-page-locators.js
@@ -2,7 +2,7 @@ module.exports = {
     datatestid: {
         signup_menu: '//*[@id="signin2"]',
         signup_modal: '//*[@id="signInModal"]',
-        signin_menu: '//*[@id="login2"]',
+        signin_menu: '//*[@id="login"]',
         username_text: '//*[@id="nameofuser"]'
     }
 }
